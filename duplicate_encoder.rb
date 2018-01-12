a48b7d6180ec70f703662692d66823655a3ea346
Ezra <ezra.doucet@gmail.com>
2018-01-12 17:07:06 -0600

    Finish Duplicate Encoder

--- duplicate_encoder.rb
+++ duplicate_encoder.rb
@@ -1,3 +1,15 @@
 def duplicate_encode(word)
+  word.downcase!
 
+  split_word = word.split("")
+  puts split_word
+
+  count = Hash.new(0)
+
+  split_word.each { |w| count[w] += 1 }
+  puts count
+
+  split_word.collect! { |x| count[x] > 1 ? ")" : "("}.join
 end
+
+puts duplicate_encode('din')
