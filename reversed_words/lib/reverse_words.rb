5472f08eff4208a99538e5e1bd82ea6d56fbd671
Ezra <ezra.doucet@gmail.com>
2018-01-12 19:11:24 -0600

    Made Tests for Reverse Words Problem.

--- reversed_words/lib/reverse_words.rb
+++ reversed_words/lib/reverse_words.rb
@@ -1,3 +1,9 @@
 class Solution
+  # def solution(sentence)
+  #   sentence.split(" ").reverse.join(" ")
+  # end
 
+  def self.reverse_word(sentence)
+    sentence.split(" ").reverse.join(" ")
+  end
 end
--- reversed_words/spec/reversed_words_spec.rb
+++ reversed_words/spec/reversed_words_spec.rb
@@ -1,6 +1,27 @@
 require "reverse_words"
 describe "Solution" do
-  it "should test for something" do
+  it "words should equal one another" do
     expect("actual").to eql("actual")
+  end
+
+  describe ".reverse_word" do
+    context "given a word" do
+      it "returns the same word" do
+        expect(Solution.reverse_word("hello")).to eql("hello")
+        expect(Solution.reverse_word("bye")).to eql("bye")
+      end
+    end
+
+    context "given two words" do
+      it "should switch the order" do
+        expect(Solution.reverse_word("hello bye")).to eql("bye hello")
+      end
+    end
+
+    context "given a long sentence" do
+      it "should reverse the order of the words in the sentence" do
+        expect(Solution.reverse_word("Bing dong king kong hello bye")).to eql("bye hello kong king dong Bing")
+      end
+    end
   end
 end
